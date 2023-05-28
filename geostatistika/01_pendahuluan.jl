c9c6172028652b2d392b06dea5535629dbef4bca
Ajam Jamaludin <jamaludinajam@gmail.com>
2023-05-28 14:20:54 +0700

    Create 01_pendahuluan.jl

---
+++ geostatistika/01_pendahuluan.jl
@@ -0,0 +1,23 @@
+### A Pluto.jl notebook ###
+# v0.19.26
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 3ddf4e30-fd27-11ed-24eb-37e1034712a0
+md"""
+# Geostatistika I: Pengantar
+## 1. Pendahuluan
+Yaudah, jadi geostatistika tuh kumpulan metode statistik yang awalnya dikembangkan buat bidang pertambangan, tapi sekarang udah dipake luas banget di banyak disiplin ilmu, kayak geofisika, geologi, geografi, dan banyak lagi. Metode-metode ini dipake buat ngeproses dan menganalisis data geografis, kayak data tentang bumi, air, cuaca, dan lain-lain.
+
+Ada juga istilah statistika yang mungkin udah sering lo denger. Statistika itu sebenernya kumpulan konsep dan metode buat ngumpulin, analisis, dan interpretasi data kuantitatif yang punya ketidakpastian. Awalnya statistika cuma dipake buat data-data negara, tapi sekarang udah jadi lebih luas, bisa dipake di banyak bidang.
+
+Di geostatistika, konsep dan metode statistika juga dipake, tapi lebih fokus ke pengolahan data yang bersifat spasial (ada hubungannya sama ruang), kayak data geofisika, gravitasi, dan data-data lain yang nyambung sama ruang dan waktu. Buat ngeproses data geostatistika ini, kita bisa pake berbagai metode, misalnya interpolasi, perataan berbobot, regresi, dan yang terkenal banget, metode kriging.
+
+Metode kriging itu tuh metode interpolasi yang diusulin oleh Matheron, seorang ahli geomatematika dari Perancis. Dia mengembangkan metode kriging untuk ngeluarin prediksi yang akurat buat variabel yang nyambung sama ruang. Setelah kriging, muncul juga metode-metode geostatistik lain yang lebih advanced, kayak Simulated Annealing, Sequential Gaussian Simulation, Bayesian Simulation, dan sebagainya.
+
+Jadi, intinya geostatistika itu gabungan antara statistika sama informasi spasial buat ngeproses dan menganalisis data yang nyambung sama ruang.
+"""
+
+# ╔═╡ Cell order:
+# ╟─3ddf4e30-fd27-11ed-24eb-37e1034712a0
